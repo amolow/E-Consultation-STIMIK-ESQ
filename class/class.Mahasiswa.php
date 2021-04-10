8899e4bee9ca899b6f1a3d153f397d13a57a8feb
a-muchsin-chalik <74577536+a-muchsin-chalik@users.noreply.github.com>
2021-04-10 15:11:53 +0700

    Update class.Mahasiswa.php

--- class/class.Mahasiswa.php
+++ class/class.Mahasiswa.php
@@ -1,2 +1,28 @@
-<html>
-</html>
+<?php
+
+class Mahasiswa {
+
+	private $nim;
+	private $prodi;
+    private $semester;
+    private $usia;
+    private $tanggalLahir;
+  
+  public function __get($atribute) 
+  {
+		if (property_exists($this, $atribute)) 
+    {
+			return $this->$atribute;
+		}
+	}
+
+	public function __set($atribut, $value)
+  {
+		if (property_exists($this, $atribut)) 
+    {
+			$this->$atribut = $value;
+		}
+	}
+
+}
+?>
